21b67cab0927f67092922c561a3a0333f2ca26c3
2240211S\2240211 <2240211@ecc.ac.jp>
2025-09-25 16:16:38 +0900

    Add Map

---
+++ Assets/Yamamoto/Map/Map.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public Transform player;
+    public GameObject chunkPrefab;
+    public int viewRadius = 2;
+    public int chunkSize = 10;
+
+    private Dictionary<Vector2Int, GameObject> chunks = new Dictionary<Vector2Int, GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2Int playerChunk = WorldToChunkPos(player.position);
+
+        for (int x = -viewRadius; x <= viewRadius; x++)
+        {
+            for (int y = -viewRadius; y <= viewRadius; y++)
+            {
+                Vector2Int coord = playerChunk + new Vector2Int(x, y);
+                if (!chunks.ContainsKey(coord))
+                {
+                    SpawnChunk(coord);
+                }
+            }
+        }
+
+        List<Vector2Int> toRemove = new List<Vector2Int>();
+        foreach (var kvp in chunks)
+        {
+            if (Mathf.Max(Mathf.Abs(kvp.Key.x - playerChunk.x), Mathf.Abs(kvp.Key.y - playerChunk.y)) > viewRadius)
+            {
+                Destroy(kvp.Value);
+                toRemove.Add(kvp.Key);
+            }
+        }
+        foreach (var key in toRemove)
+        {
+            chunks.Remove(key);
+        }
+
+    }
+
+    Vector2Int WorldToChunkPos(Vector3 pos)
+    {
+        int x = Mathf.FloorToInt(pos.x / chunkSize);
+        int y = Mathf.FloorToInt(pos.y / chunkSize);
+        return new Vector2Int(x, y);
+    }
+
+    void SpawnChunk(Vector2Int coord)
+    {
+        Vector3 worldPos = new Vector3(coord.x * chunkSize, coord.y * chunkSize, 0);
+        var chunk = Instantiate(chunkPrefab, worldPos, Quaternion.identity);
+        chunks.Add(coord, chunk);
+    }
+}
